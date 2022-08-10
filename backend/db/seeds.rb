e06e670943cdfb5bee41d8a4e2c61213c00ec3ee
satoshi-nakajima-uhuru <nakajima.satoshi@uhuru.jp>
2022-08-10 10:00:02 +0900

    :zap: create post

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |n|
+  Post.create!(
+    title: "test#{n + 1}",
+    content: "コンテンツ#{n + 1}",
+  )
+end
