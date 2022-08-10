134e7ab797a1cde00c9ec60fb136bc893459987b
satoshi-nakajima-uhuru <nakajima.satoshi@uhuru.jp>
2022-08-10 15:50:15 +0900

    test

--- backend/app/controllers/home_controller.rb
+++ backend/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ActionController::API
   def index
-    users = { id:1, name: "hoge", age: 22 }
+    users = { id:1, name: "huga", age: 22 }
     render json: users
   end
 end
